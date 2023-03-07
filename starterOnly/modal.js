255646107218b185daa0e447f7ec3289320661d9
leoleo236 <cast.leotimbert@gmail.com>
2023-03-07 19:15:48 +0100

    1 - TODO : Fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,30 @@ function editNav() {
 }
 
 // DOM Elements
+// Constante formulaire.
 const modalbg = document.querySelector(".bground");
+// Constante bouton ouverture formulaire - "je m'inscris".
 const modalBtn = document.querySelectorAll(".modal-btn");
+// Constante div contenant chaque champs formulaire.
 const formData = document.querySelectorAll(".formData");
+// Constante bouton fermeture formulaire.
+const closeBtn = document.querySelectorAll(".close");
 
-// launch modal event
+
+// 1 - TODO : FERMER LA MODALE
+/* launchModal event */
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
+// launch modal form - fonction d'ouverture du formulaire
+function launchModal() { 
   modalbg.style.display = "block";
-}
+};
+/* closeModal event  */
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+// close modal form - fonction de fermeture du formulaire
+function closeModal() { 
+  modalbg.style.display = "none";
+};
+
+
 
 
